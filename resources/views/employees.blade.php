dbaf8ac1c61ec787f563bbc0aff02b73f922a02b
Francesco <franc.ns@gmail.com>
2020-01-20 14:59:52 +0100

    added output in employees page

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.base')
 
 @section('content')
-  <h1>Employees:</h1>
+
+  <h1>Employees: {{ $employees->count() }}</h1>
+  <h3>With tasks:</h3>
+  @foreach ($employees as $employee)
+    <ul>
+      @if ($employee->tasks->count() > 0)
+        <li>{{ $employee->name }} {{ $employee->lastname }}: {{ $employee->tasks->count() }}
+          <ul>
+            @foreach ($employee->tasks as $task)
+              <li>{{ $task->id }}: {{ $task->name }}</li>
+            @endforeach
+          </ul>
+        </li>
+      @endif  
+    </ul>
+  @endforeach
+  <h3>Without tasks:</h3>
+  @foreach ($employees as $employee)
+    <ul>
+      @if ($employee->tasks->count() == 0)
+        <li>{{ $employee->name }} {{ $employee->lastname }}: {{ $employee->tasks->count() }}</li>
+      @endif
+    </ul>
+  @endforeach
+  
 @endsection
